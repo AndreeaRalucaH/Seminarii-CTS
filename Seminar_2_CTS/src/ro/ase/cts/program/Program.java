1943c15f6b219707ae08b264c8dc3fdb32c21b8c
AndreeaRalucaH <andreea_horhocia@yahoo.com>
2021-03-10 17:53:27 +0200

    Initial commit seminar 2

---
+++ Seminar_2_CTS/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
+import ro.ase.cts.clase.readers.AplicantReader;
+
+public class Program {
+
+	public static List<Aplicant> citesteaAplicanti(AplicantReader reader) throws FileNotFoundException
+	{
+		return reader.readAplicanti();
+	}
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		try {
+			listaAngajati = citesteaAplicanti(new AngajatReader("angajati.txt"));
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
