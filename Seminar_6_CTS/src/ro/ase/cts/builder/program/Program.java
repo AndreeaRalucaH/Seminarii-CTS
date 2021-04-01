7ef199a6baba9af334a6a068386ab0d40979106f
AndreeaRalucaH <andreea_horhocia@yahoo.com>
2021-04-01 16:23:23 +0300

    Builder commit

---
+++ Seminar_6_CTS/src/ro/ase/cts/builder/program/Program.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.builder.program;
+
+import ro.ase.cts.builder.clase.Rezervare;
+import ro.ase.cts.builder.clase.RezervareBuilder;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Rezervare rezervare1 = new RezervareBuilder().setCodRezervare(20).setAreBauturaInclusa(true).build();
+		Rezervare rezervare2 = new RezervareBuilder().setAreMuzicaAmbientala(true).setGenMuzica("Pop").build();
+		Rezervare rezervare3 = new Rezervare(false, true, false, false, null, 70);
+		
+		RezervareBuilder rezervareBuilder = new RezervareBuilder().setAreBauturaInclusa(true).setAreBauturaInclusa(true).setAreScaunErgonomic(true);
+		Rezervare rezervare4 = rezervareBuilder.setCodRezervare(80).build();
+		Rezervare rezervare5 = rezervareBuilder.setCodRezervare(81).build();
+		
+		System.out.println(rezervare1);
+		System.out.println(rezervare2);
+		System.out.println(rezervare3);
+		System.out.println(rezervare4);
+		System.out.println(rezervare5);
+	}
+
+}
